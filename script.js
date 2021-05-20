50f9c72482d2e6abc0f0298fe1a4ec39372b008c
Isolda Sáez Martínez <Isoldasaezmtz@MacBook-Pro-de-Isolda.local>
2021-05-20 05:10:38 +0200

    Home hasta posts relacionados

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.swiper-container', {
+  const swiperRun = new Swiper('#swiper-run', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+    autoplay: {
+      delay: 5000,
+    },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
+  });
+  const swiperPeople = new Swiper('#swiper-testimonials', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
